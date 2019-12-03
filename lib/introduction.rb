85f1addfaf9fbad1078a62bbc210d833e1ebbcd7
nickie kane <kameo712@gmail.com>
2019-12-03 05:16:29 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,17 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+name = ""
+puts name 
+
+
+introduction(nickie)
+language = ""
+puts name
+
+introduction_with_language(nickie, ruby)
