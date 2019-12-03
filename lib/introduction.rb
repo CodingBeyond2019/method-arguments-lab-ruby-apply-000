eabac09099ebc3c41673ed91990bb4fa0373248a
nickie kane <kameo712@gmail.com>
2019-12-03 05:08:00 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,14 @@
+@name = ""
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+@name = ""
+@language = ""
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name}and I am learning to program in #{language}."
+end
+
+introduction(nickie)
+
+introduction_with_language(nickie, ruby)
